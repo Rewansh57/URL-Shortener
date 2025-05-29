f3e96b48af56e4e8508bbc0124d8d93f3467aa10
Rewansh <rewansh57@gmail.com>
2025-05-29 17:40:30 +0530

    Basic file

---
+++ src/main/java/org/example/projectsh/urlshortener/UrLshortenerApplication.java
@@ -0,0 +1,14 @@
+package org.example.projectsh.urlshortener;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrLshortenerApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(UrLshortenerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/projectsh/urlshortener/UrLshortenerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.projectsh.urlshortener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrLshortenerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
